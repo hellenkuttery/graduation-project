66d0126cd42274746fe475c995c3eed73d67844f
Betulcode <helen.kut90@gmail.com>
2021-01-24 17:09:05 +0300

    Create User app/model

--- blogbackend/blogbackend/settings.py
+++ blogbackend/blogbackend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "user",
     'rest_framework',
     'blogapp',
     'django.contrib.admin',
--- blogbackend/blogbackend/urls.py
+++ blogbackend/blogbackend/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("blogapp.urls"))
+    path("",include("blogapp.urls")),
+    path("user/",include("user.urls"))
 ]
